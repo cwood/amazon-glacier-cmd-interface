0136753df3b95d9e9ebfa32231621f39a85b698f
Colin Wood <cwood06@gmail.com>
2014-12-17 10:28:01 -0800

    White space

--- glacier/GlacierWrapper.py
+++ glacier/GlacierWrapper.py
@@ -2,7 +2,7 @@
 """
 .. module:: GlacierWrapper
    :platform: Unix, Windows
-   :synopsis: Wrapper for accessing Amazon Glacier, with Amazon SimpleDB 
+   :synopsis: Wrapper for accessing Amazon Glacier, with Amazon SimpleDB
    support and other features.
 """
 
@@ -1153,7 +1153,6 @@ using %s MB parts to upload." % part_size)
 
             if not part:
                 break
-
             writer.write(part)
             current_time = time.time()
             overall_rate = int((writer.uploaded_size-start_bytes)/(current_time - start_time))
@@ -1850,7 +1849,7 @@ your archive ID is correct, and start a retrieval job using \
         if vault_names:
             vaults = vault_names.split(",")
             self._init_events_for_vaults(vaults, topic_arn)
-        
+
         topic_arns = [topic_arn]
 
         if len(topic_arns):
@@ -1872,7 +1871,7 @@ your archive ID is correct, and start a retrieval job using \
     @sns_connect
     def sns_list_topics(self, sns_options):
         topics = self.sns_conn.get_all_topics()['ListTopicsResponse']['ListTopicsResult']['Topics']
-        
+
         results = []
         for topic in topics:
             results += [{"Topic":topic['TopicArn'].split(":")[-1], "Topic ARN":topic['TopicArn']}]
